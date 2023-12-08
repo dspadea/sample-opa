c51b144c457540192c494d160b08ac90699a59c5
Dave Spadea <dspadea@users.noreply.github.com>
2023-12-07 16:50:57 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 docker build --no-cache --progress=plain -t sample-opa:latest .
 
-# test run
-docker run -p 8182:8181 sample-opa:latest
+# test run -- uncomment this for quicker iteration if you want
+# docker run -p 8182:8181 sample-opa:latest
